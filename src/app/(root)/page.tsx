0538424313868071b34bbb8244e7d1438bade8d8
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2024-01-11 22:35:11 -0500

    Initialize project

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+       <p>Hello admin dashboard</p> 
+       <Button variant="destructive">Click me lol</Button>
+    </main>
+  )
+}
